d234cec94a332a915349dc9a6cf61b8dd5480d88
hotteano <edwardchenyq@gmail.com>
2026-03-26 21:30:51 +0800

    update

--- Lab5/lab5B.cpp
+++ Lab5/lab5B.cpp
@@ -1,64 +1,112 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 0x3f3f3f3f;
+const long long INF = (1LL << 60);
 
 struct Edge {
-    int u, v, w;
+    int u, v;
+    long long w;
 };
 
-int Chu_Liu(vector<Edge> edges, int n, int rt, int pre[]) {
-    int res = 0;
-    while (true) {
-        vector<int> in(n, INF), id(n, -1), vis(n, -1);
-        vector<int> tmp_pre(n, -1);
-        
-        for (auto &e : edges) {
+long long directed_mst(int n, int root, const vector<Edge> &edges_input, vector<int> &parent) {
+    vector<Edge> edges = edges_input;
+
+    function<long long(int, int, const vector<Edge>&, vector<int>&)> solve = [&](int n_now, int rt_now, const vector<Edge> &cur_edges, vector<int> &par) -> long long {
+        vector<long long> in(n_now, INF);
+        vector<int> pre_edge(n_now, -1);
+
+        for (int i = 0; i < (int)cur_edges.size(); i++) {
+            const auto &e = cur_edges[i];
             if (e.u != e.v && e.w < in[e.v]) {
                 in[e.v] = e.w;
-                tmp_pre[e.v] = e.u; 
+                pre_edge[e.v] = i;
             }
         }
-        in[rt] = 0;
-        
-        for (int i = 0; i < n; i++) 
-            if (in[i] == INF) return -1;
-        
+
+        in[rt_now] = 0;
+        for (int i = 0; i < n_now; i++) {
+            if (in[i] == INF) return -1; // unreachable
+        }
+
+        vector<int> id(n_now, -1), vis(n_now, -1);
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            res += in[i];
+        long long add_cost = 0;
+        for (int i = 0; i < n_now; i++) add_cost += in[i];
+
+        for (int i = 0; i < n_now; i++) {
             int v = i;
-            while (vis[v] != i && id[v] == -1 && v != rt) {
+            while (vis[v] != i && id[v] == -1 && v != rt_now) {
                 vis[v] = i;
-                v = tmp_pre[v];
+                v = cur_edges[pre_edge[v]].u;
             }
-            if (v != rt && id[v] == -1) {
-                for (int u = tmp_pre[v]; u != v; u = tmp_pre[u])
-                    id[u] = cnt;
+            if (v != rt_now && id[v] == -1) {
+                for (int u = cur_edges[pre_edge[v]].u; u != v; u = cur_edges[pre_edge[u]].u) id[u] = cnt;
                 id[v] = cnt++;
             }
         }
+
         if (cnt == 0) {
-            for (int i = 0; i < n; i++) pre[i] = tmp_pre[i];
-            break;
+            par.assign(n_now, -1);
+            for (int i = 0; i < n_now; i++) {
+                if (i == rt_now) continue;
+                par[i] = cur_edges[pre_edge[i]].u;
+            }
+            return add_cost;
         }
-        
-        for (int i = 0; i < n; i++)
-            if (id[i] == -1) id[i] = cnt++;
-        
+
+        for (int i = 0; i < n_now; i++) if (id[i] == -1) id[i] = cnt++;
+
+        vector<vector<int>> comp(cnt);
+        for (int i = 0; i < n_now; i++) comp[id[i]].push_back(i);
+
         vector<Edge> new_edges;
-        for (auto &e : edges) {
+        new_edges.reserve(cur_edges.size());
+        for (const auto &e : cur_edges) {
             int u = id[e.u], v = id[e.v];
-            if (u != v) {
-                new_edges.push_back({u, v, e.w - in[e.v]});
+            if (u != v) new_edges.push_back({u, v, e.w - in[e.v]});
+        }
+
+        vector<int> sub_par;
+        long long sub_cost = solve(cnt, id[rt_now], new_edges, sub_par);
+        if (sub_cost == -1) return -1;
+
+        par.assign(n_now, -1);
+
+        for (int cid = 0; cid < cnt; cid++) {
+            if ((int)comp[cid].size() == 1) {
+                int node = comp[cid][0];
+                if (node == rt_now) par[node] = -1;
+                else par[node] = cur_edges[pre_edge[node]].u;
+                continue;
+            }
+
+            int enter_u = -1, enter_v = -1;
+            long long best = INF;
+            int parent_cid = sub_par[cid];
+            if (parent_cid != -1) {
+                for (const auto &e : cur_edges) {
+                    if (id[e.u] == parent_cid && id[e.v] == cid) {
+                        long long adjusted = e.w - in[e.v];
+                        if (adjusted < best) {
+                            best = adjusted;
+                            enter_u = e.u;
+                            enter_v = e.v;
+                        }
+                    }
+                }
+            }
+
+            for (int node : comp[cid]) {
+                if (node == enter_v) par[node] = enter_u;
+                else par[node] = cur_edges[pre_edge[node]].u;
             }
         }
-        
-        n = cnt;
-        rt = id[rt];
-        edges = new_edges;
-    }
-    return res;
+
+        par[rt_now] = -1;
+        return add_cost + sub_cost;
+    };
+
+    return solve(n, root, edges, parent);
 }
 
 int main() {
@@ -68,36 +116,47 @@ int main() {
     int n, m;
     cin >> n >> m;
     vector<Edge> edges;
+    edges.reserve(m + n);
+    long long max_w = 0;
     for (int i = 0; i < m; i++) {
         int u, v, w;
         cin >> u >> v >> w;
         edges.push_back({u, v, w});
+        max_w = max<long long>(max_w, w);
     }
-    
-    int best_rt = -1, min_cost = INF;
-    vector<int> best_pre(n);
-    
-    for (int rt = 0; rt < n; rt++) {  
-        vector<int> pre(n);
-        int cost = Chu_Liu(edges, n, rt, pre.data());
-        if (cost != -1 && cost < min_cost) {
-            min_cost = cost;
-            best_rt = rt;
-            best_pre = pre;
-        }
+
+    int super = n;
+    long long penalty = (max_w + 1) * (static_cast<long long>(n) + 1);
+    if (penalty > INF / 4) penalty = INF / 4; 
+    for (int i = 0; i < n; i++) edges.push_back({super, i, penalty});
+
+    vector<int> parent;
+    long long total_cost = directed_mst(n + 1, super, edges, parent);
+
+    if (total_cost == -1) {
+        cout << "impossible" << '\n';
+        return 0;
     }
-    
-    if (best_rt == -1) {
-        cout << "impossible" << endl;
-    } else {
-        cout << min_cost << " " << best_rt << endl;
-        for (int i = 0; i < n; i++) {
-            if (i != best_rt) {
-                cout << i << " " << best_pre[i] << endl;
-            }
-            else{
-                cout << i << endl;
-            }
+
+    vector<int> roots;
+    for (int i = 0; i < n; i++) {
+        if (parent[i] == super) roots.push_back(i);
+    }
+
+    if (roots.size() != 1) {
+        cout << "impossible" << '\n';
+        return 0;
+    }
+
+    int best_root = roots[0];
+    long long answer_cost = total_cost - penalty; 
+
+    cout << answer_cost << " " << best_root << '\n';
+    for (int i = 0; i < n; i++) {
+        if (i == best_root) {
+            cout << i << '\n';
+        } else {
+            cout << i << " " << parent[i] << '\n';
         }
     }
     return 0;
